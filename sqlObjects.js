c20d4eac9b943525619fc2ec101ccf895a6958b8
Alex Georgiou <georgiua@gmail.com>
2018-10-21 21:14:34 +0300

    tokens moved to config, sqlObjects added

---
+++ sqlObjects.js
@@ -0,0 +1,24 @@
+function Tweet(id, user, topic, tweet) {
+  this.id = id;
+  this.user = user;
+  this.topic = topic;
+  this.tweet = tweet;
+}
+
+function Followers(id, user, followers) {
+  this.id = id;
+  this.user = user;
+  this.followers = followers;
+}
+
+function Friends(id, user, friends) {
+  this.id = id;
+  this.user - user;
+  this.friends = friends;
+}
+
+function Ids(id, user, user_id) {
+  this.id = id;
+  this.user = user;
+  this.user_id = user_id;
+}
