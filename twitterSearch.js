6d4330b16b80af0d9c0ad62b993411b11753e907
Alex Georgiou <georgiua@gmail.com>
2018-10-27 16:38:32 +0300

    cron jobs

--- twitterSearch.js
+++ twitterSearch.js
@@ -4,29 +4,33 @@ var config = require('./config.js');
 var dbSettings = require('./dbSettings.js');
 var sqlObjects = require('./sqlObjects.js');
 var mysql = require('mysql');
+const CronJob = require('cron').CronJob;
+const querystring = require('querystring');
 
 // setting up the objects
 var client = new Twitter(config);
 var con = mysql.createConnection(dbSettings);
-var topics = ['node.js', 'MAGAbomber', ''];
-
-// db connect
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+var topics = ['node.js', 'MAGAbomber', 'WorldSeries18'];
+var maxIds = {
+  'node.js': 0,
+  'MAGAbomber': 0,
+  'WorldSeries18': 0,
+}
 
 // function andri
 function searchTwitter(topic, maxId) {
+  console.log("in searchTwitter");
   const parameters = {
     q: topic,
     result_type:'recent',
     count: '100'
   };
+  var counter = 1;
 
-  if(maxId) {
-    parameters.max_id = maxId;
+  if(maxIds[topic] != 0) {
+    parameters.max_id = maxIds[topic];
   }
+  console.log('Parameters' + parameters);
 
   client.get('search/tweets', parameters, function(error, tweets, response) {
     if(error) throw error;
@@ -43,14 +47,26 @@ function searchTwitter(topic, maxId) {
         console.log("1 new record inserted");
       });
     });
-    var maxId = tweets.max_id;
-    searchTwitter(topic, maxId);
-  }
+    var next_results = querystring.parse(tweets.search_metadata.next_results);
+    maxIds[topic] = next_results.max_id;
+    counter++;
+    if(counter <= 60) {
+        searchTwitter(topic, maxId);
+    }
+  });
 }
 
-// main loop to collect data
-topics.forEach(function(topic) {
-  searchTwitter(topic);
+// schedule the job
+const job = new CronJob('0 0/16 * 1/1 * * *', function() {
+
+  // main loop to collect data
+  topics.forEach(function(topic) {
+    searchTwitter(topic);
+  });
 });
 
-con.end();
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  job.start();
+});
